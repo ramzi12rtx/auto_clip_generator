43ea6a2c4dd583a571ea3f49c1745715edc45aa5
ramzi12rtx <ughggvbg@gmail.com>
2025-07-27 11:38:50 +0100

    Update downloader.py

--- utils/downloader.py
+++ utils/downloader.py
@@ -1,20 +1,15 @@
-import subprocess
-import os
-import uuid
-
 def download_youtube_video(url):
-    filename = f"assets/video_{uuid.uuid4().hex[:8]}.mp4"
     try:
-        os.makedirs("assets", exist_ok=True)
-        cmd = [
-            "yt-dlp",
+        output_path = f"assets/video_{uuid.uuid4().hex[:8]}.mp4"
+        command = [
+            "yt-dlp", "--no-playlist",
             "-f", "mp4",
-            "-o", filename,
+            "--no-check-certificate",  # ✅ يتخطى مشاكل SSL
+            "-o", output_path,
             url
         ]
-        subprocess.run(cmd, check=True)
-        print(f"✅ تم تحميل الفيديو: {filename}")
-        return filename
+        subprocess.run(command, check=True)
+        return output_path
     except Exception as e:
         print("❌ Error downloading video:", e)
         return None
